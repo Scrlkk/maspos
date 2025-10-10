3d88766e3770eda78e7a832e9bfce21e79e6ccf9
Scrlkk <shevaazki6@gmail.com>
2025-10-10 16:50:44 +0700

    backup -Category BE Fixed

--- resources/js/Components/CategoryCols.vue
+++ resources/js/Components/CategoryCols.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 
 <template>
     <section
-        :class="`flex items-center px-5 py-2 cursor-pointer ${category.bgColor} rounded-lg ${category.textColor}`"
+        :class="`flex items-center px-5 py-2 cursor-pointer ${category.bgColor} rounded-lg ${category.textColor} transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg whitespace-nowrap`"
     >
         {{ category.nama }}
     </section>
